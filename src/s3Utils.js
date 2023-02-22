103f9a8a622e8885ae72a51a358e9ba6a93ab5a1
Dan Janosik <dan@47.io>
2023-02-22 16:17:29 -0500

    logging fix

--- src/s3Utils.js
+++ src/s3Utils.js
@@ -20,7 +20,7 @@ const createBucket = (bucket, pathPrefix, bucketOptions={}) => {
 		s3Client = new AWS.S3({apiVersion: '2006-03-01'});
 	}
 
-	debugLog(`Creating S3 Bucket: bucket=${bucket}, pathPrefix=${pathPrefix}, bucketOptions=${bucketOptions}`);
+	debugLog(`Creating S3 Bucket: bucket=${bucket}, pathPrefix=${pathPrefix}, bucketOptions=${JSON.stringify(bucketOptions)}`);
 
 	let prefix = (pathPrefix || "").trim();
 	if (prefix.length > 0 && !prefix.endsWith("/")) {
